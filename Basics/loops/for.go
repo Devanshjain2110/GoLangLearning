001d6cb998674741aa3b868d6b9f97546a0be93e
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2026-05-09 15:14:42 +0530

    Basic understanding of how for loops work in golang

---
+++ Basics/loops/for.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+
+
+func main(){
+
+	// Basic Loop
+	i := 0
+ for i<=3 {
+	fmt.Println(i)
+	i++
+}
+
+// Conditional loop
+for j:=0; j<3; j++{
+	fmt.Println(j)
+}
+
+// Range loop
+for i := range 4 {
+	fmt.Println("Range : ", i)
+}
+
+// Continuation and break
+for n := range 6 {
+	if n%2 == 0 {
+		continue
+	}
+	fmt.Println(n)
+}
+
+}
+
